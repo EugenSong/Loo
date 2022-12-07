cdfaf6699dfc422dfa96a261afaa6b2102c27f83
Eugene <75242911+EugenSong@users.noreply.github.com>
2022-12-06 17:20:18 -0800

    fetched API data and stored within explorer

--- Loo/Controllers/RestroomExplorerController.swift
+++ Loo/Controllers/RestroomExplorerController.swift
@@ -8,21 +8,29 @@
 import UIKit
 
 class RestroomExplorerController: UIViewController {
-
+    
     // ~1 - Properties
     let api = RestroomAPI()
+    
+    var restroomsArray: [Restroom] = []
     
     // ~2 - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemMint
-        
-        RestroomAPI.getRestroomAPI(lat: 34.052339, lon: -118.239505)
 
+        getRestroomsAPIData()
     }
     
     // ~3 - Helpers
     
-
+    func getRestroomsAPIData() {
+        RestroomAPI.getRestroomAPI(lat: 34.052339, lon: -118.239505) { (restrooms) in guard let restrooms = restrooms else { return }
+            self.restroomsArray = restrooms
+            print("getRestroomsAPIData() called")
+            print(self.restroomsArray)
+            
+        }
+    }
 }
--- Loo/Model/RestroomAPI.swift
+++ Loo/Model/RestroomAPI.swift
@@ -9,15 +9,15 @@ import Foundation
 
 struct RestroomAPI {
     
-    let restrooms: [Restroom]? = []
-    
     // how to grab API
-    static func getRestroomAPI(lat: Float, lon: Float) {
+    static func getRestroomAPI(lat: Float, lon: Float, completion: @escaping ([Restroom]?) -> Void) {
         
         // declare URL
         guard let url = URL(string: "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=5&offset=0&lat=\(lat)&lng=\(lon)") else {
             return
         }
+        
+        var restrooms: [Restroom] = []
         
         // create task
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
@@ -30,14 +30,25 @@ struct RestroomAPI {
                 do {
                     let tasks = try decoder.decode([Restroom].self, from: data)
                     tasks.forEach { restroom in
-                        print(restroom.name!)
-                        print(restroom)
+//                        print(restroom.name!)
+//                        print(restroom)
+                        restrooms.append(restroom)
                     }
+//                    print("restrooms is the following array: \(restrooms)")
+                    
                 } catch {
                     print(error.localizedDescription)
                 }
             }
+            return completion(restrooms)
         }
         task.resume()
     }
 }
+
+/*
+ *** IMPORTANT CONCEPT ON USING COMPLETION HANDLERS TO FULFILL API DATA FETCHING ***
+ 
+ Q: Why do I need to create an RestroomAPI file? And then create another func to call it again in Controller?
+ A: To call the API endpoint and then store data in an array within this file
+ A: Then later, in my Controller, create another func and call the completion handler within it to set the array from this file into another array there... ultimate goal: to pass data from API endpoint --> this file medium --> controller  */
