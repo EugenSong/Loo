f87c791fe14e55fcba7aad7050b940673ad0ebe1
Eugene <75242911+EugenSong@users.noreply.github.com>
2022-12-26 16:13:22 -0800

    updated infoplist data purpose string

--- Loo/SceneDelegate.swift
+++ Loo/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
